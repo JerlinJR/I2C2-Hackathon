f019e945cfbc15a47c4313ff121010016a574b7b
stainsleto <stainsmault@gmail.com>
2023-03-26 22:01:37 +0530

    @stainsleto/basic

--- 5ire/src/App.jsx
+++ 5ire/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
+import Home from './components/home/Home'
 
 import './App.css'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello Jerlin, Hope you know the struggle of Watering GIT 
-    </h1>
+    <Home />
   )
 }
 
