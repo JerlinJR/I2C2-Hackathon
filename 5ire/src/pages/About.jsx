ad7941203c1b6d0e03faacaa015717271f268172
stainsleto <stainsmault@gmail.com>
2023-03-30 23:55:43 +0530

    @stainsleto/about page started

--- 5ire/src/pages/About.jsx
+++ 5ire/src/pages/About.jsx
@@ -9,7 +9,7 @@ export default function About(){
             <div className='all flex flex-row justify-center items-center mx-36'>
                 <div>
                     <h1 className="text-pink-500 font-bold text-5xl my-2">About us</h1>
-                    <p>Building a sustainable future through distributed ledger technology.
+                    <p className='w-96'>Building a sustainable future through distributed ledger technology.
                     The 5ire ecosystem aims to bring a shift in the paradigm from a for-profit economy to afor-benefit economy.
                     </p>
                     <button>Contact us</button>
