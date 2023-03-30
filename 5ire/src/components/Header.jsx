637fa1d17d89248c0172179077bbefe40332c8e1
stainsleto <stainsmault@gmail.com>
2023-03-31 00:07:06 +0530

    header text added

--- 5ire/src/components/Header.jsx
+++ 5ire/src/components/Header.jsx
@@ -9,16 +9,15 @@ export default function Header(){
               <a href=""> <img src="src/assets/logo.png" alt="logo-bg" /> </a> 
               </div>
               <div id='buttons'>
-                <button id='btn1'> <a href=""><img src="src/assets/AboutUs.png" alt="aboutus" /></a></button>
-                <button id='btn2'> <a href=""><img src="src/assets/1testnet.png" alt="testnet" /></a></button>
-                <button id='btn3'> <a href=""><img src="src/assets/1community.png" alt="community" /></a></button>
-                <button id='btn4'> <a href=""><img src="src/assets/1research.png" alt="research" /></a></button>
-                <button id='btn5'> <a href=""><img src="src/assets/1validator.png" alt="" /></a></button>
-                <button id='btn6'> <a href=""><img src="src/assets/sustainability.png" alt="" /></a></button>
+                <button id='btn1'>About us</button>
+                <button id='btn2'>Test net</button>
+                <button id='btn3'>Community</button>
+                <button id='btn4'>Research</button>
+                <button id='btn5'>Sustainability</button>
                 </div>
                 <div id='irebutton'>
-                <a href=""> <img src="src/assets/5ire Explorer-png.png" alt="5ire-Explorer-Logo"  /></a>
-              </div>
+                  <button id='btn6'>5ire Explore</button>
+                </div>
             </header>
     
             </div>
