bb3fd4f0c495e594995d2958d12d92a78d4a4729
JerlinJR <infodotjerlin@gmail.com>
2023-04-02 00:46:44 +0530

    Advisor Not completed

--- 5ire/src/components/Advisor.jsx
+++ 5ire/src/components/Advisor.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
-
+import '../Advisor.css'
+import Logo from '../assets/logo.png'
 export default function Advisor(){
     return(
         <>
-        
+
+            <div class='advisor'>
+                <div class="title">
+                    <h1>Advisors</h1>
+                </div>
+            <div class="row">
+            <div class="column">
+                <div class="card">
+                    <div class="image-bg"></div>
+                    <div class="linkedin"> <h1>in</h1></div>
+                    <img src="https://www.w3schools.com/howto/img_avatar.png"/>
+                <h3>Jeff Hoffman</h3>
+                <p>Global Enterpreneur,</p>
+                <p>Founder Priceline.com</p>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="card">
+                <div class="image-bg"></div>
+                    <div class="linkedin"> <h1>in</h1></div>
+                    <img src="https://www.w3schools.com/howto/img_avatar.png"/>
+                <h3>Jeff Hoffman</h3>
+                <p>Global Enterpreneur,</p>
+                <p>Founder Priceline.com</p>
+                </div>
+            </div>
+            
+            <div class="column">
+                <div class="card">
+                <div class="image-bg"></div>
+                    <div class="linkedin"> <h1>in</h1></div>
+                    <img src="https://www.w3schools.com/howto/img_avatar.png"/>
+                <h3>Jeff Hoffman</h3>
+                <p>Global Enterpreneur,</p>
+                <p>Founder Priceline.com</p>
+                </div>
+            </div>
+            
+            <div class="column">
+                <div class="card">
+                <div class="image-bg"></div>
+                    <div class="linkedin"> <h1>in</h1></div>
+                    <img src="https://www.w3schools.com/howto/img_avatar.png"/>
+                <h3>Jeff Hoffman</h3>
+                <p>Global Enterpreneur,</p>
+                <p>Founder Priceline.com</p>
+                </div> 
+             </div>
+            </div>
+            </div>
+
         </>
     )
 }
